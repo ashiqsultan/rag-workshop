147c76a3eb083500d5e85e52909cef412c4b83ed
AshiqSultan <ashiq.sultan@astrazeneca.com>
2025-01-18 11:44:01 +0530

    removed redundant schema definition in main file

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ from app.notes.semantic_search import semantic_search
 from app.temp_kb import temp_kb
 from app.notes.rag_answer import rag_answer
 from app.helpers.sqlite_db import init_db, get_all, get_by_id
+from app.notes.schema import NotesCreate
 
 app = FastAPI()
 
@@ -60,12 +61,8 @@ async def test_split_text(reqbody: ChatMessage):
     return {"data": result}
 
 
-class CreateNotes(BaseModel):
-    note: str
-
-
 @app.post("/notes", tags=["Notes"])
-async def create_notes(reqbody: CreateNotes):
+async def create_notes(reqbody: NotesCreate):
     note = reqbody.note
     created = await create(note)
     return {"data": created}
@@ -76,14 +73,11 @@ async def get_all_notes():
     notes = get_all()
     return notes
 
+
 @app.get("/notes/{note_id}", tags=["Notes"])
 async def get_note_by_id(note_id: str):
     note = get_by_id(note_id)
     return note
-
-
-class GetSimilarNotes(BaseModel):
-    text: str
 
 
 @app.get("/notes/get-similar", tags=["Notes"])
