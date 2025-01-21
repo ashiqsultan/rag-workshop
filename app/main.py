f708a9f8b7b76b8ebee23387755e3651116704c9
AshiqSultan <ashiq.sultan@astrazeneca.com>
2025-01-21 22:46:33 +0530

    Added dotenv

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
+from dotenv import load_dotenv, find_dotenv
 import os
 from fastapi import FastAPI
 from pydantic import BaseModel
+
+
+load_dotenv(find_dotenv())
+
 
 app = FastAPI()
 
@@ -17,3 +22,9 @@ class ChatMessage(BaseModel):
 @app.post("/chat")
 async def chat(reqbody: ChatMessage):
     return {"message": "Hello this is test. This is your message: " + reqbody.user_message}
+
+
+@app.get("/app-name")
+async def get_app_name():
+    app_name = os.getenv("APP_NAME", "DefaultAppName")
+    return {"app_name": app_name}
